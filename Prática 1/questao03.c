0829784e0689c3b281bdf6f0d577a9a2ce0bb560
thiagocsv <92699494+thiagocsv@users.noreply.github.com>
2022-03-09 18:24:47 -0300

    Questão 03

---
+++ Prática 1/questao03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int r;
+  float pi = 3.1416;
+  printf("Medida do raio: ");
+  scanf("%d", &r);
+  float per = 2*pi*r;
+  printf("A medida do perímeto é de %.1f", per);
+}
