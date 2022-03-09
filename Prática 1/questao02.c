41aa910d166e2fb1e13893825277b824b0ed5cb4
thiagocsv <92699494+thiagocsv@users.noreply.github.com>
2022-03-09 18:12:56 -0300

    Questão 02

---
+++ Prática 1/questao02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int base, height;
+  printf("Medida da base: ");
+  scanf("%d", &base);
+  printf("Medida da base: ");
+  scanf("%d", &height);
+  int area = ((base*height)/2);
+  printf("Sua área é de %d", area);
+}
