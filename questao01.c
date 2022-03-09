97b50ff0a71b3c20aa8d2978f43b2018a09ca495
thiagocsv <92699494+thiagocsv@users.noreply.github.com>
2022-03-09 17:58:21 -0300

    Create questao01.c

---
+++ questao01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  float A1, A2;
+  printf("Nota A1: ");
+  scanf("%f", &A1);
+  printf("Nota A2: ");
+  scanf("%f", &A2);
+  float media = (0.4*A1) + (0.6*A2);
+  printf("Sua media é %.1f", media);
+  return 0;
+}
