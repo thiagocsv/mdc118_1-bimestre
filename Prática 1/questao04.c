eaa8a8f08c301843a8f9e529496afb020b92be73
thiagocsv <92699494+thiagocsv@users.noreply.github.com>
2022-03-10 18:28:57 -0300

    Questão 04

---
+++ Prática 1/questao04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  float initial, ICMS = 0.17, COFINS = 0.076, PIS_PASEP = 0.0165;
+  printf("Valor do produto: ");
+  scanf("%f", &initial);
+  float final_price = (1 + ICMS + COFINS + PIS_PASEP) * initial;
+  printf("Valor com impostos incluidos: %.2f", final_price);
+  return 0;
+}
